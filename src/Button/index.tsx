e43d4895bb80a70845a25db07fef41ad2099f8cf
malikov.vladimir <malikov.vladimir@itmh.ru>
2020-12-25 17:19:40 +0500

    Initial commit

---
+++ src/Button/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { StyledButton } from './style';
+
+const Button: React.FC = () => {
+  return (
+    <StyledButton>Example</StyledButton>
+  );
+}
+
+export default Button;
---
+++ src/Button/style.ts
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const StyledButton = styled.button`
+  display: flex;
+  width: 100%;
+  height: 51px;
+  background: blue;
+`;
+
