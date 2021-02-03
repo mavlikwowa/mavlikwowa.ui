053e36c24dd9efbfe0a1c481d96b5756d7cc0afa
malikov.vladimir <malikov.vladimir@itmh.ru>
2021-02-03 16:43:03 +0500

    Changed "burger" icon

--- src/Icons/BurgerIcon/index.tsx
+++ src/Icons/BurgerIcon/index.tsx
@@ -3,31 +3,14 @@ import React from 'react';
 const BurgerIcon: React.FC = () => {
   return (
     <svg
-      width="18px"
-      height="12px"
-      viewBox="0 0 18 12"
-      version="1.1"
+      width="26"
+      height="19"
+      viewBox="0 0 26 19"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g id="Icons" strokeWidth="1" fillRule="evenodd">
-        <g id="Two-Tone" transform="translate(-817.000000, -3438.000000)">
-          <g id="Navigation" transform="translate(100.000000, 3378.000000)">
-            <g
-              id="Two-Tone-/-Navigation-/-menu"
-              transform="translate(714.000000, 54.000000)"
-            >
-              <g>
-                <polygon id="Path" points="0 0 24 0 24 24 0 24" />
-                <path
-                  d="M3,18 L21,18 L21,16 L3,16 L3,18 Z M3,13 L21,13 L21,11 L3,11 L3,13 Z M3,6 L3,8 L21,8 L21,6 L3,6 Z"
-                  id="🔹-Primary-Color"
-                  fill="#1D1D1D"
-                />
-              </g>
-            </g>
-          </g>
-        </g>
-      </g>
+      <path d="M0 0H26V3H0V0Z" />
+      <path d="M0 8H26V11H0V8Z" />
+      <path d="M0 16H26V19H0V16Z" />
     </svg>
   );
 };
